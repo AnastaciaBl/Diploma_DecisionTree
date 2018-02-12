768419aadb8772334e039d91d154dc6d4fc5aab2
AnastaciaBl <AnastaciaBl96@gmail.com>
2018-02-13 00:36:59 +0200

    Added main classes and methods signature

---
+++ DecisionTree/DecisionTree/DecisionTree.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecisionTree
+{
+    class DecisionTree
+    {
+        public DecisionTreeNode Head { get; set; }
+
+        public DecisionTree(List<string> trainingSampleLines)
+        {
+
+        }
+
+        private void Learn(List<double> args)
+        {
+            //build the tree using CART algorithm
+        }
+
+        public void Deside()
+        {
+            //give an answer
+        }
+
+        private void AddNode()
+        {
+            //add a new node
+        }
+
+        private void CutUselessNodes()
+        {
+            //improve the tree
+            //cut leaves which doesn`t have a lot of influence on result
+        }
+    }
+}
---
+++ DecisionTree/DecisionTree/DecisionTreeNode.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecisionTree
+{
+    class DecisionTreeNode
+    { 
+        public DecisionTreeNode RightChild { get; set; } //the rule was executed
+        public DecisionTreeNode LeftChild { get; set; } //the rule wasn`t executed
+        public bool IsLeaf { get; set; }
+        public int AmountOfElements { get; set; }//how much elements this node has inside
+        private double[] Rule { get; set; } //[0] - index of argument; [1] - index of element`s position
+
+        public DecisionTreeNode()
+        {
+            Rule = new double[2];
+        }
+
+        private bool Compute(double rule)
+        {
+            //compute which side has a new child: left or right
+            return true;
+        }
+
+        private double[] FindDefiningArgument()
+        {
+            //find argument which has the smallest squares error`s difference 
+            return new double[2];
+        }
+    }
+}
