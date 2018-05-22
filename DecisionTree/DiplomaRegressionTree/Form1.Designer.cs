4a4a70040fd6391c5aa5d69dc5d4d663c1ba3371
Anastasiia Blyzniuk <ablyz@softserveinc.com>
2018-05-22 18:55:39 +0300

    Add tree view. It does not work.

--- DecisionTree/DiplomaRegressionTree/Form1.Designer.cs
+++ DecisionTree/DiplomaRegressionTree/Form1.Designer.cs
@@ -28,23 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series9 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series10 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.btnOpenFile = new System.Windows.Forms.Button();
             this.RegressionChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.treeView = new System.Windows.Forms.TreeView();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.btnDecide = new System.Windows.Forms.Button();
+            this.panelPaint = new System.Windows.Forms.Panel();
+            this.pictureBoxPaintTree = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.RegressionChart)).BeginInit();
+            this.panelPaint.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPaintTree)).BeginInit();
             this.SuspendLayout();
             // 
             // btnOpenFile
             // 
-            this.btnOpenFile.Location = new System.Drawing.Point(13, 348);
+            this.btnOpenFile.Location = new System.Drawing.Point(841, 504);
             this.btnOpenFile.Name = "btnOpenFile";
-            this.btnOpenFile.Size = new System.Drawing.Size(99, 23);
+            this.btnOpenFile.Size = new System.Drawing.Size(163, 49);
             this.btnOpenFile.TabIndex = 0;
             this.btnOpenFile.Text = "Open file...";
             this.btnOpenFile.UseVisualStyleBackColor = true;
@@ -52,42 +55,35 @@
             // 
             // RegressionChart
             // 
-            chartArea1.Name = "ChartArea1";
-            this.RegressionChart.ChartAreas.Add(chartArea1);
-            this.RegressionChart.Location = new System.Drawing.Point(329, 12);
+            chartArea5.Name = "ChartArea1";
+            this.RegressionChart.ChartAreas.Add(chartArea5);
+            this.RegressionChart.Location = new System.Drawing.Point(793, 28);
             this.RegressionChart.Name = "RegressionChart";
-            series1.ChartArea = "ChartArea1";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
-            series1.Name = "CorrelationField";
-            series2.BorderColor = System.Drawing.Color.White;
-            series2.BorderWidth = 3;
-            series2.ChartArea = "ChartArea1";
-            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StepLine;
-            series2.Color = System.Drawing.Color.Red;
-            series2.Name = "RegressionLine";
-            this.RegressionChart.Series.Add(series1);
-            this.RegressionChart.Series.Add(series2);
+            series9.ChartArea = "ChartArea1";
+            series9.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series9.Name = "CorrelationField";
+            series10.BorderColor = System.Drawing.Color.White;
+            series10.BorderWidth = 3;
+            series10.ChartArea = "ChartArea1";
+            series10.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StepLine;
+            series10.Color = System.Drawing.Color.Red;
+            series10.Name = "RegressionLine";
+            this.RegressionChart.Series.Add(series9);
+            this.RegressionChart.Series.Add(series10);
             this.RegressionChart.Size = new System.Drawing.Size(370, 300);
             this.RegressionChart.TabIndex = 1;
             this.RegressionChart.Text = "chart1";
             // 
-            // treeView
-            // 
-            this.treeView.Location = new System.Drawing.Point(13, 13);
-            this.treeView.Name = "treeView";
-            this.treeView.Size = new System.Drawing.Size(310, 299);
-            this.treeView.TabIndex = 2;
-            // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(241, 333);
+            this.textBox1.Location = new System.Drawing.Point(841, 361);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 3;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(241, 373);
+            this.textBox2.Location = new System.Drawing.Point(841, 387);
             this.textBox2.Name = "textBox2";
             this.textBox2.ReadOnly = true;
             this.textBox2.Size = new System.Drawing.Size(100, 20);
@@ -95,7 +91,7 @@
             // 
             // btnDecide
             // 
-            this.btnDecide.Location = new System.Drawing.Point(383, 348);
+            this.btnDecide.Location = new System.Drawing.Point(947, 372);
             this.btnDecide.Name = "btnDecide";
             this.btnDecide.Size = new System.Drawing.Size(75, 23);
             this.btnDecide.TabIndex = 5;
@@ -103,20 +99,41 @@
             this.btnDecide.UseVisualStyleBackColor = true;
             this.btnDecide.Click += new System.EventHandler(this.btnDecide_Click);
             // 
+            // panelPaint
+            // 
+            this.panelPaint.AutoScroll = true;
+            this.panelPaint.Controls.Add(this.pictureBoxPaintTree);
+            this.panelPaint.Location = new System.Drawing.Point(3, 3);
+            this.panelPaint.Name = "panelPaint";
+            this.panelPaint.Size = new System.Drawing.Size(784, 573);
+            this.panelPaint.TabIndex = 6;
+            // 
+            // pictureBoxPaintTree
+            // 
+            this.pictureBoxPaintTree.Location = new System.Drawing.Point(3, 3);
+            this.pictureBoxPaintTree.Name = "pictureBoxPaintTree";
+            this.pictureBoxPaintTree.Size = new System.Drawing.Size(760, 550);
+            this.pictureBoxPaintTree.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBoxPaintTree.TabIndex = 0;
+            this.pictureBoxPaintTree.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(711, 416);
+            this.ClientSize = new System.Drawing.Size(1175, 588);
+            this.Controls.Add(this.panelPaint);
             this.Controls.Add(this.btnDecide);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.treeView);
             this.Controls.Add(this.RegressionChart);
             this.Controls.Add(this.btnOpenFile);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.RegressionChart)).EndInit();
+            this.panelPaint.ResumeLayout(false);
+            this.panelPaint.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPaintTree)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -126,10 +143,11 @@
 
         private System.Windows.Forms.Button btnOpenFile;
         private System.Windows.Forms.DataVisualization.Charting.Chart RegressionChart;
-        private System.Windows.Forms.TreeView treeView;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button btnDecide;
+        private System.Windows.Forms.Panel panelPaint;
+        private System.Windows.Forms.PictureBox pictureBoxPaintTree;
     }
 }
 
